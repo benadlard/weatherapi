b08f9b91b6c630e1c083c27368942ca28d7fbaff
Ben Adlard <benjamin.adlard@mmtdigital.co.uk>
2023-04-20 10:51:20 +0100

    created tf storage and container resources

--- main.tf
+++ main.tf
@@ -5,18 +5,41 @@ provider "azurerm" {
 
 terraform {
     backend "azurerm" {
-        resource_group_name  = "tf_rg_storage"
+        resource_group_name  = "storage-rg"
         storage_account_name = "benadlardtfstorage"
         container_name       = "tfstate"
         key                  = "terraform.tfstate"
     }
 }
 
-resource "azurerm_resource_group" "resource_group" {
-    name = "tfmainrg"
+# resource groups
+resource "azurerm_resource_group" "main-rg" {
+    name = "main-rg"
     location = "UK South"
 }
 
+resource "azurerm_resource_group" "storage-rg" {
+    name     = "storage-rg"
+    location = "UK South"
+}
+
+# storage container groups
+
+resource "azurerm_storage_account" "tfstate-sa" {
+    name                     = "benadlardtfstorage"
+    resource_group_name      = azurerm_resource_group.storage-rg.name
+    location                 = azurerm_resource_group.storage-rg.location
+    account_tier             = "Standard"
+    account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "tfstate-container" {
+    name                  = "tfstate"
+    storage_account_name  = azurerm_storage_account.tfstate-sa.name
+    container_access_type = "private"
+}
+
+# container resource
 resource "azurerm_container_group" "container_group" {
     name                  = "weatherapi"
     location              = azurerm_resource_group.resource_group.location
