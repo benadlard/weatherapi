273b5f908df1e25bd0b7188db14e0e0c56cbaae3
Ben Adlard <benjamin.adlard@mmtdigital.co.uk>
2023-04-20 11:07:05 +0100

    reverted previous changes

--- main.tf
+++ main.tf
@@ -18,29 +18,8 @@ resource "azurerm_resource_group" "main-rg" {
     location = "UK South"
 }
 
-resource "azurerm_resource_group" "storage-rg" {
-    name     = "storage-rg"
-    location = "UK South"
-}
-
-# storage container groups
-
-resource "azurerm_storage_account" "tfstate-sa" {
-    name                     = "benadlardtfstorage"
-    resource_group_name      = azurerm_resource_group.storage-rg.name
-    location                 = azurerm_resource_group.storage-rg.location
-    account_tier             = "Standard"
-    account_replication_type = "LRS"
-}
-
-resource "azurerm_storage_container" "tfstate-container" {
-    name                  = "tfstate"
-    storage_account_name  = azurerm_storage_account.tfstate-sa.name
-    container_access_type = "private"
-}
-
 # container resource
-resource "azurerm_container_group" "container_group" {
+resource "azurerm_container_group" "container-group" {
     name                  = "weatherapi"
     location              = azurerm_resource_group.resource_group.location
     resource_group_name   = azurerm_resource_group.resource_group.name
